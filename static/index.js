4fc63ca61c607703e1c29c50bd61328c6ee10fb8
YawOtuo <yotuo2003@gmail.com>
2022-10-03 23:26:05 +0000

    changing url editting paragraphs

---
+++ static/index.js
@@ -0,0 +1,47 @@
+window.onload = (event) => {
+    fadeIn()
+
+    setTimeout(slideOut, 2500)
+    setTimeout(slideOut, 2500)
+
+    hide_wait_small()
+    setTimeout(show_wait_small, 1500)
+
+
+}
+
+fadeIn =() => {
+    homerow = document.getElementById('homepic')
+    console.log(homerow)
+    homerow.classList.add("animate__animated")
+    homerow.classList.add("animate__slow")
+
+    homerow.classList.add("animate__fadeIn")
+
+}
+
+slideOut = () => {
+    homerow = document.getElementById('homepic')
+    console.log(homerow)
+    homerow.classList.add("animate__animated")
+    homerow.classList.add("animate__slow")
+
+    $('.homepic').slideUp(1000)
+    // homerow.classList.add('d-none')
+}
+
+remove = () => {
+    homerow = document.getElementById('homepic')
+    homerow.classList.add('d-none')
+
+}
+
+hide_wait_small = ( ) => {
+    wait_small  = document.getElementById('wait_small')
+    wait_small.classList.add('d-none')
+}
+
+show_wait_small = () => {
+    wait_small  = document.getElementById('wait_small')
+    wait_small.classList.add('d-block')
+}
